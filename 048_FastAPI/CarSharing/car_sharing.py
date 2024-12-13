99b1231d2b38d326bb6b7b0b4445ffbdb71c1a82
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-12-13 12:20:07 +0530

    Day 10

--- 048_FastAPI/CarSharing/car_sharing.py
+++ 048_FastAPI/CarSharing/car_sharing.py
@@ -3,6 +3,17 @@ from fastapi.responses import FileResponse
 from datetime import datetime
 import os
 
+db = [
+    {"id": 1, "size": "s", "fuel": "petrol",    "doors": 3, "transmission": "auto"  },
+    {"id": 2, "size": "s", "fuel": "electric",  "doors": 3, "transmission": "auto"  },
+    {"id": 3, "size": "s", "fuel": "petrol",    "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric",  "doors": 3, "transmission": "auto"  },
+    {"id": 5, "size": "m", "fuel": "hybrid",    "doors": 5, "transmission": "auto"  },
+    {"id": 6, "size": "m", "fuel": "petrol",    "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "disel",     "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric",  "doors": 5, "transmission": "auto"  },
+    {"id": 9, "size": "l", "fuel": "hybrid",    "doors": 5, "transmission": "auto"  }
+]
 
 app = FastAPI()
 
