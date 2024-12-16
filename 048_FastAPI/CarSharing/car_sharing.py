0089268f7b925856b0e2eb95cd0b9733f8da06c6
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-12-17 00:40:06 +0530

    Day 12 - Using APIRouter to reorganise the code

--- 048_FastAPI/CarSharing/car_sharing.py
+++ 048_FastAPI/CarSharing/car_sharing.py
@@ -1,35 +1,17 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from fastapi.responses import FileResponse
-from fastapi import HTTPException
-from fastapi import Depends
-from datetime import datetime
-import os
-from sqlmodel import SQLModel, Session, create_engine, select
 
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel
 import uvicorn
 
-import sys
-# sys.path.insert(0, os.path.dirname(__file__))
-# print(sys.path)
 
-from schemas import CarDbModel, CarInput
-from schemas import CarOutput
-from schemas import TripInput, TripOutput
-from schemas import TripDbModel
-from schemas import load_lib
-from schemas import save_lib
+from db import engine
 
+import sys, os
+sys.path.insert(0, os.path.dirname(__file__))
 
-db = load_lib()
-
-
-engine = create_engine(
-    "sqlite:///carsharing.db",
-    connect_args={"check_same_thread": False},
-    echo=True
-)
-
+from routers import cars
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -43,38 +25,7 @@ async def lifespan(app:FastAPI):
     print("That's all folks!")
 
 app = FastAPI(title="Car Sharing App", description="An app to share cars.", lifespan=lifespan)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-
-
-@app.get("/api/cars")
-def get_cars(size: str|None = None, doors: int|None = None, session: Session = Depends(get_session))->list[CarOutput]:  #->list[CarDbModel]:   #
-    """Returns a list of cars from the server records"""
-    query = select(CarDbModel)
-    if size:
-        query = query.where(CarDbModel.size == size)
-    if doors:
-        query = query.where(CarDbModel.doors == doors)
-
-    result = session.exec(query).all()
-
-    if result:
-        return result
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with size={size} and doors={doors}.")
-
-
-@app.get("/api/cars/{id}")
-def get_car_by_id(id: int, session: Session = Depends(get_session))->CarOutput:         # ->list[CarDbModel]:   #
-    """Returns a car matching the provided 'id' from the server records"""
-    car = session.get(CarDbModel, id)
-    if car:
-        return car
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id {id} found on the server.")
+app.include_router(cars.router)
 
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
@@ -82,88 +33,6 @@ async def favicon():
     path  = os.path.join(os.path.dirname(__file__), 'static', 'favicon.ico')
     print(path)
     return FileResponse(path)
-
-@app.post("/api/cars", response_model=CarOutput)
-def add_car(car: CarInput, session: Session = Depends(get_session)) -> CarOutput:
-    new_car = CarDbModel.model_validate(car)
-    session.add(new_car)
-    session.commit()
-    session.refresh(new_car)
-    return new_car
-
-
-@app.delete("/api/cars/{id}", status_code=204)
-def remove_car(id: int, session: Session = Depends(get_session)) -> None:
-    car = session.get(CarDbModel, id)
-    if car:
-        session.delete(car)
-        session.commit()
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id {id} found.")
-
-## Modify the preexisting object of a Car, by id
-@app.put("/api/cars/{id}", response_model=CarOutput)
-def change_car(id: int, new_data: CarInput, session: Session = Depends(get_session)) -> CarOutput:
-    car = session.get(CarDbModel, id)
-    if car:
-        car.fuel = new_data.fuel
-        car.size = new_data.size
-        car.doors = new_data.doors
-        car.transmission = new_data.transmission
-
-        session.commit()
-        return car
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
-
-
-# Add trip to an existing Car object
-@app.post("/api/cars/{car_id}/trips", response_model=TripOutput)
-def add_trip(car_id: int, trip: TripInput, session: Session = Depends(get_session)) -> TripOutput:
-    car = session.get(CarDbModel, car_id)
-    if car:
-        new_trip = TripDbModel.model_validate(trip, update={'car_id': car.id, 'id': None})
-        car.trips.append(new_trip)
-        session.commit()
-        session.refresh(new_trip)
-        return new_trip
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id {car_id} found.")
-
-@app.delete("/api/cars/{car_id}/trips/{trip_id}", status_code=204)
-def remove_trip(car_id: int, trip_id:int, session: Session = Depends(get_session)) -> None:
-    car = session.get(CarDbModel, car_id)
-    if car:
-        for trip in car.trips:
-            if trip.id == trip_id:
-                session.delete(trip)
-                session.commit()
-                return 
-        else:
-            raise HTTPException(status_code=404, detail=f"No trip with id {trip_id} found.")
-    else:
-        raise HTTPException(status_code=404, detail=f"No car with id {car_id} found.")
-
-## Migrate the data from the json file to the Database ########################
-
-@app.post("/api/cars/migrate")
-def migrate_data_to_db(session: Session = Depends(get_session)) -> list[CarOutput]:
-    lstCars = []
-    for car in db:
-        new_car = CarDbModel.model_validate(car, update={'trips': []})
-        session.add(new_car)
-
-        session.commit()
-
-        session.refresh(new_car)
-        for trip in car.trips:
-            new_trip = TripDbModel.model_validate(trip, update={'car_id': new_car.id, 'id': None})
-            session.add(new_trip)
-            session.commit()
-            session.refresh(new_car)
-        lstCars.append(new_car)
-    return lstCars
-
 
 
 if __name__ == "__main__":
---
+++ 048_FastAPI/CarSharing/routers/cars.py
@@ -0,0 +1,124 @@
+from fastapi import HTTPException
+from fastapi import Depends
+from fastapi import APIRouter
+from sqlmodel import Session, select
+
+from schemas import CarDbModel, CarInput
+from schemas import CarOutput
+from schemas import TripInput, TripOutput
+from schemas import TripDbModel
+from schemas import load_lib
+
+from db import get_session
+
+db = load_lib()
+
+router = APIRouter(prefix="/api/cars")
+
+
+@router.get("/")
+def get_cars(size: str|None = None, doors: int|None = None, session: Session = Depends(get_session))->list[CarOutput]:  #->list[CarDbModel]:   #
+    """Returns a list of cars from the server records"""
+    query = select(CarDbModel)
+    if size:
+        query = query.where(CarDbModel.size == size)
+    if doors:
+        query = query.where(CarDbModel.doors == doors)
+
+    result = session.exec(query).all()
+
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with size={size} and doors={doors}.")
+
+
+@router.get("/{id}")
+def get_car_by_id(id: int, session: Session = Depends(get_session))->CarOutput:         # ->list[CarDbModel]:   #
+    """Returns a car matching the provided 'id' from the server records"""
+    car = session.get(CarDbModel, id)
+    if car:
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id {id} found on the server.")
+
+@router.post("/", response_model=CarOutput)
+def add_car(car: CarInput, session: Session = Depends(get_session)) -> CarOutput:
+    new_car = CarDbModel.model_validate(car)
+    session.add(new_car)
+    session.commit()
+    session.refresh(new_car)
+    return new_car
+
+
+@router.delete("/{id}", status_code=204)
+def remove_car(id: int, session: Session = Depends(get_session)) -> None:
+    car = session.get(CarDbModel, id)
+    if car:
+        session.delete(car)
+        session.commit()
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id {id} found.")
+
+## Modify the preexisting object of a Car, by id
+@router.put("/{id}", response_model=CarOutput)
+def change_car(id: int, new_data: CarInput, session: Session = Depends(get_session)) -> CarOutput:
+    car = session.get(CarDbModel, id)
+    if car:
+        car.fuel = new_data.fuel
+        car.size = new_data.size
+        car.doors = new_data.doors
+        car.transmission = new_data.transmission
+
+        session.commit()
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+# Add trip to an existing Car object
+@router.post("/{car_id}/trips", response_model=TripOutput)
+def add_trip(car_id: int, trip: TripInput, session: Session = Depends(get_session)) -> TripOutput:
+    car = session.get(CarDbModel, car_id)
+    if car:
+        new_trip = TripDbModel.model_validate(trip, update={'car_id': car.id, 'id': None})
+        car.trips.append(new_trip)
+        session.commit()
+        session.refresh(new_trip)
+        return new_trip
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id {car_id} found.")
+
+@router.delete("/{car_id}/trips/{trip_id}", status_code=204)
+def remove_trip(car_id: int, trip_id:int, session: Session = Depends(get_session)) -> None:
+    car = session.get(CarDbModel, car_id)
+    if car:
+        for trip in car.trips:
+            if trip.id == trip_id:
+                session.delete(trip)
+                session.commit()
+                return 
+        else:
+            raise HTTPException(status_code=404, detail=f"No trip with id {trip_id} found.")
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id {car_id} found.")
+
+## Migrate the data from the json file to the Database ########################
+
+@router.post("/migrate")
+def migrate_data_to_db(session: Session = Depends(get_session)) -> list[CarOutput]:
+    lstCars = []
+    for car in db:
+        new_car = CarDbModel.model_validate(car, update={'trips': []})
+        session.add(new_car)
+
+        session.commit()
+
+        session.refresh(new_car)
+        for trip in car.trips:
+            new_trip = TripDbModel.model_validate(trip, update={'car_id': new_car.id, 'id': None})
+            session.add(new_trip)
+            session.commit()
+            session.refresh(new_car)
+        lstCars.append(new_car)
+    return lstCars
